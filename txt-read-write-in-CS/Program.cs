1743c109280ed66486899e8f2fc7d484868cf904
tomas BLACK <tomas_black@icloud.com>
2022-03-30 21:30:11 +0200

    Projektdateien hinzufügen.

---
+++ txt-read-write-in-CS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace write_to_a_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteText();
+            ReadText();
+
+            Console.ReadKey();
+        }
+
+        public static void WriteText()
+        {
+            string path = "C:\\CS-Files\\file.txt";
+            //string Text = "By Tomas Black.";
+            string Text = @"Headline
+Textline 1
+Textline 2
+Textline 3";
+            File.WriteAllText(path, Text);
+        }
+
+        public static void ReadText()
+        {
+            string path = "C:\\CS-Files\\file.txt";
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+        }
+    }
+}
